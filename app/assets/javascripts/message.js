feb1684b1b22dc3f5660eee590a9d7ae5fb6a1c9
tech-camp <tech-camp@tech-campnoMacBook-Air-2.local>
2019-08-21 15:50:25 +0900

    uodate

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,45 +1,26 @@
 $(function(){
   function buildHTML(message){
-    if ( message.image ) {
-    var html = 
-      `<div class="main--space" data-message-id=${message.id}>
-        <div class="main--space__box">
-          <div class="main--space__box__user">
-            ${message.user_name}
+    image = ( message.image ) ? `<img class= "lower-message__image" src=${message.image} > </img>` : "";
+      var html = 
+        `<div class="main--space">
+          <div class="main--space__box">
+            <div class="main--space__box__user">
+              ${message.user_name}
+            </div>
+            <div class="main--space__box__time">
+              ${message.date}
+            </div>
           </div>
-        <div class="main--space__box__time">
-          ${message.date}
-        </div>
-       </div>
-      <div class="main--space__box__message">
-        <p class="lower-message__content">
-          ${message.content}
-        </p>
-      </div>
-      <asset_path src=${message.image} >
+          <div class="main--space__box__message">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+          </div>
+        ${image}
      </div>`
     return html;
-  } else {
-    var html =
-    `<div class="main--space" data-message-id=${message.id}>
-       <div class="main--space__box">
-         <div class="main--space__box__user">
-           ${message.user_name}
-         </div>
-         <div class="main--space__box__time">
-           ${message.date}
-         </div>
-       </div>
-       <div class="main--space__box__message">
-         <p class="lower-message__content">
-           ${message.content}
-         </p>
-       </div>
-     </div>`
-   return html;
- };
-}
-  $('.new_message').on('submit', function(){
+  }
+  $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
@@ -50,18 +31,19 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
-  })
-  .done(function(data){
-    var html = buildHTML(data);
-    $('.messages').append(html)
-    $('.input--erea__box__comment').val('')
-    $('.main--spaces').animate({ scrollTop: $('.main--spaces')[0].scrollHeight},'fast');
-    $('form')[0].reset();
-    // location.href = url + position;
-  })
-  .fail(function(){
-    alert('error');
-  });
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      console.log("テスト");
+      $('.main--spaces').append(html);
+      $('.input--erea__box__comment').val('')
+      $('.main--spaces').animate({ scrollTop: $('.main--spaces')[0].scrollHeight})
+      // $('form')[0].reset();
+      // location.href = url + position;
+    })
+    .fail(function(){
+      alert('error');
+    });
     return false;
   });
 })
